dea40f24a6584a728782ebaa213492017aa97bc6
David Trejo LT <trejo.m.david98@gmail.com>
2024-10-07 09:32:47 -0500

    finished file setup and completed delete for authors

--- src/app/page.js
+++ src/app/page.js
@@ -2,43 +2,12 @@
 
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { Button } from 'react-bootstrap';
-import { getBooks } from '../api/bookData';
-import { useAuth } from '../utils/context/authContext';
-import BookCard from '../components/BookCard';
+import React from 'react';
 
-function Home() {
-  // TODO: Set a state for books
-  const [books, setBooks] = useState([]);
-
-  // TODO: Get user ID using useAuth Hook
-  const { user } = useAuth();
-
-  // TODO: create a function that makes the API call to get all the books
-  const getAllTheBooks = () => {
-    getBooks(user.uid).then(setBooks);
-  };
-
-  // TODO: make the call to the API to get all the books on component render
-  useEffect(() => {
-    getAllTheBooks();
-  }, []);
-
+export default function Hpme() {
   return (
-    <div className="text-center my-4">
-      <Link href="/book/new" passHref>
-        <Button>Add A Book</Button>
-      </Link>
-      <div className="d-flex flex-wrap">
-        {/* TODO: map over books here using BookCard component */}
-        {books.map((book) => (
-          <BookCard key={book.firebaseKey} bookObj={book} onUpdate={getAllTheBooks} />
-        ))}
-      </div>
+    <div className="container">
+      <h2>Welcome to Simply Books!</h2>
     </div>
   );
 }
-
-export default Home;
